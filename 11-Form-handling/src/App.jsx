982fbfd2db35ee2029f9e00e6cfc3c104fd59223
Nitesh Kumar <niteshkumar4y@gmail.com>
2026-03-15 20:43:03 +0530

    Form Handling

---
+++ 11-Form-handling/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    console.log('Form Submitted...')
+
+  }
+
+  return (
+    <div>
+      <form onSubmit= {(e)=>{
+        submitHandler(e)
+      }}>
+
+        <input type="text" placeholder='Enter Text Here....'/>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App

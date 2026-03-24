2826ed295691551a7cc94aee618e00320ed2100b
Nitesh Kumar <niteshkumar4y@gmail.com>
2026-03-24 13:07:02 +0530

    Bootstrap

---
+++ 13-Notes-App/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form className='flex justify-between'>
+        <input type="text" placeholder='Enter Notes Heading' />
+        <textarea placeholder='Enter Details' name="" id=""></textarea>
+      </form>
+    </div>
+  )
+}
+
+export default App
